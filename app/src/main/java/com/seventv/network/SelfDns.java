bb598d37b8f16593f7f90c1b53c4b3f66171eb45
Mannix <jun2675731483@gmail.com>
2020-02-25 16:44:13 +0800

    dns cache

--- app/src/main/java/com/seventv/network/SelfDns.java
+++ app/src/main/java/com/seventv/network/SelfDns.java
@@ -7,15 +7,23 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import fuckdns.Fuckdns;
 import okhttp3.Dns;
 
 public class SelfDns implements Dns {
+
+    private HashMap<String, List<InetAddress>> cache = new HashMap<>();
+
     @Override
     public List<InetAddress> lookup(String hostname) throws UnknownHostException {
         if (hostname == null) throw new UnknownHostException("hostname == null");
+
+        if (cache.containsKey(hostname)) {
+            return cache.get(hostname);
+        }
 
         String ips = Fuckdns.loopup(hostname, "114.114.114.114:53", "202.67.240.222:53", "doh.bigbuckbunny.xyz:47.90.117.50");
 
@@ -23,6 +31,8 @@ public class SelfDns implements Dns {
         for (String ip : ips.split(";")) {
             addresses.add(InetAddress.getByName(ip));
         }
+
+        cache.put(hostname, addresses);
 
         return addresses;
     }
