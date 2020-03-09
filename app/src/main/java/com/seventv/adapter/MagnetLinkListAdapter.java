0ede31494cd603f026f9b672ccdf568f1b3e9761
GaoMjun <katjoj-dyxze3-hufsyM>
2020-03-09 21:17:38 +0800

    no message

--- app/src/main/java/com/seventv/adapter/MagnetLinkListAdapter.java
+++ app/src/main/java/com/seventv/adapter/MagnetLinkListAdapter.java
@@ -21,6 +21,9 @@ public class MagnetLinkListAdapter extends BaseQuickAdapter<MagnetLink, BaseView
     @Override
     protected void convert(@NonNull BaseViewHolder helper, MagnetLink item) {
         helper.setText(R.id.magnet_link_title, item.getTitle());
+        helper.setText(R.id.magnet_link_url, item.getLink());
+        helper.setText(R.id.magnet_link_size, item.getSize());
+        helper.setText(R.id.magnet_link_date, item.getDate());
         helper.setOnClickListener(R.id.magnet_link_card_view, v -> {
 
             Log.d("MagnetLinkListAdapter", item.getLink());
